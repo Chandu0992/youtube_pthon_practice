8d995ee5de61cb9b34df278a0332bf38b68e0f12
Chandu0992 <30655263+Chandu0992@users.noreply.github.com>
2018-11-18 11:07:47 +0530

    Update for_loop.py

--- for_loop.py
+++ for_loop.py
@@ -1,2 +1,32 @@
-for i in range(101):
+#x = ['Chandu',25,3.5]
+#x = [2,6,"Vaishu",100]
+#for i in x:
+'''
+for i in range(10):
     print(i)
+'''
+'''
+for i in range(11,21,2): #range(start,end,step)
+    print(i)'''
+'''
+for i in range(20,10,-1):
+    if i%5!=0:
+        print(i)
+'''
+
+#Assignment
+# Print all the perfect squar number between 1 and 500
+
+for i in range(1,501):
+    j = 1
+    while j*j <= i:
+        if (j*j) == i:
+            print(i)
+        j +=1
+
+
+
+
+
+
+
